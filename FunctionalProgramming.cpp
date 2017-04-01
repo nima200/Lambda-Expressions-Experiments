41665126361eb0b04b8d3bf29995c8392849afd8
Nima <nima.adibpour@mail.mcgill.ca>
2017-04-01 11:02:44 -0400

    Documented code.

--- FunctionalProgramming.cpp
+++ FunctionalProgramming.cpp
@@ -4,27 +4,59 @@
 #include <functional>
 
 using namespace std;
+
+ /**
+  * Base class for the rest of the comparers to inherit from.
+  */
 class IComparer {
 public:
     virtual bool isBefore(int a, int b) {}
 };
+/**
+ * Class for less than comparison
+ */
 class LessThanComparer : public IComparer {
 public:
+    /**
+     * Checks if the first number is smaller than the second number
+     * @param a The first number
+     * @param b The second number
+     * @return True if a &lt b, false otherwise.
+     */
     bool isBefore(int a, int b) {
         return a < b;
     }
 };
+/**
+ * Class for greater than comparison
+ */
 class GreaterThanComparer : public IComparer {
 public:
+    /**
+     * Checks if the first number is greater than the second number
+     * @param a The first number
+     * @param b The second number
+     * @return True if a &gt b, false otherwise.
+     */
     bool isBefore(int a, int b) {
         return a > b;
     }
 };
+/**
+ * Class for distance comparison between the two numbers provided against
+ * a center stored privately in the class
+ */
 class IsClosestToComparer : public IComparer {
 private:
     int center;
 public:
     IsClosestToComparer(int center) : center(center) {}
+    /**
+     * Checks which of the two provided numbers are closer to the center.
+     * @param a The first number
+     * @param b The second number
+     * @return <strong>True</strong> if a is closer to the center than b, <strong>false otherwise</strong>.
+     */
     bool isBefore(int a, int b) {
         return abs(a - center) < abs(b - center);
     }
@@ -55,15 +87,6 @@ bool isLessThan(int a, int b);
  * the result of comparison as <strong>true</strong> or <strong>false</strong>.
  */
 typedef bool (*compFunction)(int, int);
-/**
- * @brief Bubble sort meant for use with an array (vector) that uses
- * a function pointer, whose function is called on comparison of two
- * values to decide whether the sort has to be ascending or descending
- * @param p         Reference to the first element of the array to be sorted
- * @param length    The length of the array to be sorter
- * @param compare   Pointer to function to be called on each comparison on
- *                  two elements of the array
- */
 void(sortWithFunctionPointers(int *p, int length, compFunction compare));
 void sortWithInterfaces(int *p, int length, IComparer* compare);
 void sortWithStandardFunction(int *p, int length, function<bool (int, int)> compare);
@@ -82,12 +105,17 @@ int main() {
     testSortWithLambdas();
     return 0;
 }
+/**
+ * Test function that demonstrates the features of sorting using function pointers.
+ */
 void testSortPointers() {
     cout << "Testing sort with Function Pointers" << endl << endl;
     unsigned long arraySize = 10;
+    // Creating two arrays as we will be testing two ways of sorting here.
     vector<int> myArray1(arraySize);
     vector<int> myArray2(arraySize);
     cout << "Unsorted array: " << setw(20) << "\t";
+    // Printing the array before being sorted by any function.
     for (int i = 0; i < arraySize; i++) {
         myArray2[i] = myArray1[i] = rand() % arraySize + 1;
         cout << myArray1[i] << " ";
@@ -95,23 +123,31 @@ void testSortPointers() {
     cout << endl;
     cout << "Sorted array: " << endl;
     cout << "With isLessThan: " << endl << setw(35) << "\t";
+    // Calling onto the respective sort function for this test, with pointer to "isLessThan" function passed.
     sortWithFunctionPointers(&myArray1[0], arraySize, isLessThan);
+    // Printing out the result of the sorted array
     for (int i = 0; i < arraySize; ++i) {
         cout << myArray1[i] << " ";
     }
     cout << endl;
     cout << "With isGreaterThan: " << endl << setw(35) << "\t";
+    // Calling onto the respective sort function for this test, with pointer to "isGreaterThan" function passed.
     sortWithFunctionPointers(&myArray2[0], arraySize, isGreaterThan);
+    // Printing out the result of the sorted array.
     for (int i = 0; i < arraySize; ++i) {
         cout << myArray2[i] << " ";
     }
     cout << endl << endl;
 }
+/**
+ * Test function that demonstrates the features of sorting using inheritance from an interface.
+ */
 void testSortWithInterfaces() {
     cout << "Testing sort with interfaces" << endl << endl;
     cout << "LessThanComparer: " << endl;
     unsigned long arraySize = 10;
     vector<int> myArray1(arraySize);
+    // Printing out the unsorted array.
     cout << "Unsorted array: " << setw(20) << "\t";
     for (int i = 0; i < arraySize; i++) {
         myArray1[i] = rand() % arraySize + 1;
@@ -119,12 +155,16 @@ void testSortWithInterfaces() {
     }
     cout << endl;
     cout << "Sorted array: " << setw(20) << "\t";
+    // Calling to the respective sort function of this test with a new instance of the "LessThanComparer" as
+    // The interface that we pass for sorting.
     sortWithInterfaces(&myArray1[0], arraySize, new LessThanComparer());
+    // Printing out the sorted array as a result of our sort function call.
     for (int j = 0; j < arraySize; ++j) {
         cout << myArray1[j] << " ";
     }
     cout << endl << endl << "GreaterThanComparer: " << endl;
     vector<int> myArray2(arraySize);
+    // Printing the unsorted array.
     cout << "Unsorted array: " << setw(20) << "\t";
     for (int i = 0; i < arraySize; i++) {
         myArray2[i] = rand() % arraySize + 1;
@@ -132,13 +172,16 @@ void testSortWithInterfaces() {
     }
     cout << endl;
     cout << "Sorted array: " << setw(20) << "\t";
+    // Calling to the respective sort function of this test with a new instance of the "GreaterThanComparer" as
+    // The interface that we pass for sorting.
     sortWithInterfaces(&myArray2[0], arraySize, new GreaterThanComparer());
+    // Printing the sorted array as a result of our sort function call.
     for (int j = 0; j < arraySize; ++j) {
         cout << myArray2[j] << " ";
     }
-
     cout << endl << endl << "IsClosestToComparer with center 3: " << endl;
     vector<int> myArray3(arraySize);
+    // Printing the unsorted array.
     cout << "Unsorted array:" << setw(20) << "\t";
     for (int i = 0; i < arraySize; i++) {
         myArray3[i] = rand() % arraySize + 1;
@@ -146,17 +189,25 @@ void testSortWithInterfaces() {
     }
     cout << endl;
     cout << "Sorted array:" << setw(20) << "\t";
-         sortWithInterfaces(&myArray3[0], arraySize, new IsClosestToComparer(3));
+    // Calling to the respective sort function of this test with a new instance of the "IsClosestToComparer" as
+    // The interface that we pass for sorting, with a center value of 3.
+    sortWithInterfaces(&myArray3[0], arraySize, new IsClosestToComparer(3));
+    // Printing the sorted array as a result of our sort function call.
     for (int j = 0; j < arraySize; ++j) {
         cout << myArray3[j] << " ";
     }
     cout << endl << endl;
 }
+/**
+ * Test function that demonstrates the features of sorting with lambda expressions
+ */
 void testSortWithLambdas() {
     cout << "Testing sort with Lambda Expressions" << endl << endl;
     unsigned long arraySize = 10;
+    // Two arrays for testing two different lambda expressions, very similar to each other however.
     vector<int> myArray1(arraySize);
     vector<int> myArray2(arraySize);
+    // Printing out the unsorted array.
     cout << "Unsorted array: " << setw(20) << "\t";
     for (int i = 0; i < arraySize; i++) {
         myArray2[i] = myArray1[i] = rand() % arraySize + 1;
@@ -164,11 +215,16 @@ void testSortWithLambdas() {
     }
     cout << endl << "Sorted array: " << endl;
     cout << "Center value 3: " << endl << setw(35) << "\t";
+    // Calling the respective sort function for this demonstration, passing 3 as center value to the
+    // Lambda expression that we have generated in the function "generateNearestTo(center)".
     sortWithStandardFunction(&myArray1[0], arraySize, generateNearestTo(3));
+    // Printing out the result of
     for (int i = 0; i < arraySize; ++i) {
         cout << myArray1[i] << " ";
     }
     cout << endl << "Center value 10: " << endl << setw(35) << "\t";
+    // Calling the respective sort function for this demonstration, passing 10 as center value to the
+    // Lambda expression that we have generated in the function "generateNearestTo(center)".
     sortWithStandardFunction(&myArray2[0], arraySize, generateNearestTo(10));
     for (int i = 0; i < arraySize; ++i) {
         cout << myArray2[i] << " ";
@@ -176,14 +232,31 @@ void testSortWithLambdas() {
     cout << endl;
 }
 
+/**
+ * Function that checks if the first number is smaller than the second number or not
+ * @param a The first number
+ * @param b The second number
+ * @return <strong>True</strong> if a &lt b, <strong>false</strong> otherwise.
+ */
 bool isLessThan(int a, int b) {
     return a < b;
 }
 
+/**
+ * Function that checks if the first number is greater than the second number or not
+ * @param a The first number
+ * @param b The second number
+ * @return <strong>True</strong> if a &gt b, <strong>False</strong> otherwise.
+ */
 bool isGreaterThan(int a, int b) {
     return a > b;
 }
-
+/**
+ * Bubble sort with function pointers.
+ * @param p Pointer to the first element of the array
+ * @param length Length of the array
+ * @param compare Function to call on two elements for comparison
+ */
 void sortWithFunctionPointers(int *p, int length, compFunction compare) {
     for (int i = 0; i < length; i++) {
         for (int j = 0; j < length - 1 - i; j++) {
@@ -195,7 +268,12 @@ void sortWithFunctionPointers(int *p, int length, compFunction compare) {
         }
     }
 }
-
+/**
+ * Bubble sort with interfaces
+ * @param p Pointer to the first element of the array
+ * @param length Length of the array
+ * @param compare Pointer to the interface class that contains the compare method we use on two elements of the array
+ */
 void sortWithInterfaces(int *p, int length, IComparer *compare) {
     for (int i = 0; i < length; ++i) {
         for (int j = 0; j < length - 1 - i; ++j) {
@@ -207,14 +285,24 @@ void sortWithInterfaces(int *p, int length, IComparer *compare) {
         }
     }
 }
-
+/**
+ * Lambda expression generator
+ * @param center The center value to check the other values against
+ * @return A lambda expression that takes in two numbers and checks if the first number is closer to the center than
+ * the second
+ */
 function<bool(int, int)> generateNearestTo(int center) {
     auto compare = [center](int a, int b) {
         return abs(a - center) < abs(b - center);
     };
     return compare;
 }
-
+/**
+ * Bubble sort with lambda expressions
+ * @param p Pointer to the first element of the array
+ * @param length The length of the array
+ * @param compare The lambda expression expected in order to perform a comparison on two values of the array with.
+ */
 void sortWithStandardFunction(int *p, int length, function<bool(int, int)> compare) {
     for (int i = 0; i < length; ++i) {
         for (int j = 0; j < length - 1 - i; ++j) {
